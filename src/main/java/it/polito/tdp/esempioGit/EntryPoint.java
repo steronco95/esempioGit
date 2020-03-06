b2daf3d0ef24242d9eede6ba57a9fe08b7896333
djonk <djonk@DESKTOP-MNTGEEC.homenet.telecomitalia.it>
2020-03-06 10:37:47 +0100

    commento inutile

--- src/main/java/it/polito/tdp/esempioGit/EntryPoint.java
+++ src/main/java/it/polito/tdp/esempioGit/EntryPoint.java
@@ -22,6 +22,8 @@ public class EntryPoint extends Application {
         stage.show();
     }
 
+    //ciaone belli!
+    
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
